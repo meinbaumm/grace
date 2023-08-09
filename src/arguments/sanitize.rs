e36a0927b475d894c1f9fdcb4d811e7ad1cfe440
meinbaumm <maksim.petrenko@aviasales.ru>
2023-08-09 12:55:11 +0700

    add arguments module for working with command line arguments

---
+++ src/arguments/sanitize.rs
@@ -0,0 +1,9 @@
+use crate::case::string::sanitize;
+
+pub fn maybe_sanitize(file_name: String, is_sanitize: &bool) -> String {
+    if *is_sanitize {
+        sanitize(file_name.as_str())
+    } else {
+        file_name
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod arguments;
 pub mod case;
 pub mod file;
